6c28175273214f82565af96cfd9b31f32a9df1b3
jjiny <jjiny3773@naver.com>
2022-05-11 23:49:06 +0900

    Fix: afterMount관련 오류 수정
    - render가 아닌 라이프사이클 메서드까지 observe되는 현상 수정
    - afterMount 라이프사이클 메서드와 isMountFinished 업데이트의 순서를 바꿈

--- Vanilla JS로 SPA 만들기/src/core/Component.js
+++ Vanilla JS로 SPA 만들기/src/core/Component.js
@@ -50,7 +50,7 @@ export default class Component {
   beforeUpdate() {}
   afterUpdate() {}
   beforeUnmount() {}
-  
+
   update(newTarget) {
     if (newTarget && newTarget !== this.target) {
       this.target = newTarget;
@@ -73,9 +73,11 @@ export default class Component {
     if (this.isMountFinished) this.afterUpdate();
 
     if (!this.isMountFinished) {
-      this.setEvents();
-      this.afterMount();
-      this.isMountFinished = true;
+      setTimeout(() => {
+        this.setEvents();
+        this.isMountFinished = true;
+        this.afterMount();
+      }, 0);
     }
   }
 
